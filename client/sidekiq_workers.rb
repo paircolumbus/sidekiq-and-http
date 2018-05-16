9fedb999aa2287ea6bf653e50645c1ad403c05a3
Andrew Bonner <abonner@covermymeds.com>
2018-05-16 14:29:51 -0400

    DONE. :D

--- client/sidekiq_workers.rb
+++ client/sidekiq_workers.rb
@@ -8,11 +8,10 @@ class GetRequestSender
   sidekiq_retry_in { 0 }
 
   def perform(path, params={})
-    # For exercise 3, replace this comment with code that
-    # sends the request, parses the response, and uses `puts` to 
-    # print the message part of the response
+    begin
+      response = HttpConnection.get(path, query: params)
+    end until response.code == 200
 
-    # For exercise 5, replace this comment with code that
-    # retries the request if it fails
+    puts JSON.parse(response)["message"]
   end
 end
