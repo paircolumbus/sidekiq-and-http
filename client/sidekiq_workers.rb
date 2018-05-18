c5a4eddf5eb8f5ac1498d3a6486f0cca35ad7fe3
Elizabeth Callam <ecallam@covermymeds.com>
2018-05-18 10:13:42 -0400

    first commit

--- client/sidekiq_workers.rb
+++ client/sidekiq_workers.rb
@@ -1,5 +1,6 @@
 require 'sidekiq'
 require_relative 'http_connection'
+require 'json'
 Sidekiq::Logging.logger = nil
 
 class GetRequestSender
@@ -9,8 +10,20 @@ class GetRequestSender
 
   def perform(path, params={})
     # For exercise 3, replace this comment with code that
-    # sends the request, parses the response, and uses `puts` to 
+    # sends the request, parses the response, and uses `puts` to
     # print the message part of the response
+
+      loop do
+        response = HttpConnection.get(path, query: params)
+        if response.code == 200
+          parsed_json = JSON.parse(response)
+          puts parsed_json["message"]
+          break
+        end
+      end
+
+
+
 
     # For exercise 5, replace this comment with code that
     # retries the request if it fails
